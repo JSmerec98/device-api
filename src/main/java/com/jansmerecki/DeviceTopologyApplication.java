953aadc7c2ad3a89d6d894117a048a8ca94d8d74
JSmerec98 <jasiek.smerecki@gmail.com>
2025-09-20 08:38:21 +0200

    Add spring dependencies and modify main application class

---
+++ src/main/java/com/jansmerecki/DeviceTopologyApplication.java
@@ -0,0 +1,12 @@
+package com.jansmerecki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceTopologyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeviceTopologyApplication.class, args);
+    }
+}
